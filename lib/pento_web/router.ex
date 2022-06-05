e5dc6325beda95cbfde3d8555101da8a1d6e4b87
Olaleye Blessing <olayinkablexxy@gmail.com>
2022-06-05 10:43:16 +0100

    chapter 3

--- lib/pento_web/router.ex
+++ lib/pento_web/router.ex
@@ -82,6 +82,13 @@ defmodule PentoWeb.Router do
     # Please, search for `live_session` in this file to read about it
     live_session :default, on_mount: PentoWeb.UserAuthLive do
       live "/guess", WrongLive
+
+      live "/products", ProductLive.Index, :index
+      live "/products/new", ProductLive.Index, :new
+      live "/products/:id/edit", ProductLive.Index, :edit
+
+      live "/products/:id", ProductLive.Show, :show
+      live "/products/:id/show/edit", ProductLive.Show, :edit
     end
 
     get "/users/settings", UserSettingsController, :edit
