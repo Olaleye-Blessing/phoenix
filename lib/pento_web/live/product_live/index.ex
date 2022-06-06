399cad227937ca2710cf6dce3bf47870696ad77b
Olaleye Blessing <olayinkablexxy@gmail.com>
2022-06-06 07:17:02 +0100

    chapter 4

--- lib/pento_web/live/product_live/index.ex
+++ lib/pento_web/live/product_live/index.ex
@@ -6,7 +6,18 @@ defmodule PentoWeb.ProductLive.Index do
 
   @impl true
   def mount(_params, _session, socket) do
-    {:ok, assign(socket, :products, list_products())}
+    # {:ok, assign(socket, :products, list_products())}
+
+    {:ok,
+      socket
+      |> assign(:products, list_products())
+    }
+
+    # {:ok,
+    #  assign(
+    #    socket,
+    #    products: list_products()
+    #  )}
   end
 
   @impl true
